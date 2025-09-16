e4ca9f8df9300e36ad1406913bd30e3dc7003b50
Erwann QUEVIT <quevit.erwann@mail-ecv.fr>
2025-09-16 16:19:13 +0200

    creation book

--- src/Domain/BookRepositoryInterface.php
+++ src/Domain/BookRepositoryInterface.php
@@ -1,6 +1,10 @@
 <?php
 namespace App\Domain;
+use App\Domain\Entity\Book;
+
 Interface BookRepositoryInterface
 {
     public function get();
+
+    public function add($title, $author, $published_date, $isbn , $category, $status, $price, $owner);
 }
--- src/Infrastructure/BookRepository.php
+++ src/Infrastructure/BookRepository.php
@@ -26,6 +26,26 @@ class BookRepository extends ServiceEntityRepository implements BookRepositoryIn
         $result = $statement->executeQuery();
         return $result->fetchAllAssociative();
     }
+
+    public function add($title, $author, $isbn, $published_date, $category, $status, $price, $owner)
+    {
+        $sql = "INSERT INTO books (title, author, isbn, published_date, category, status, price, owner)
+                VALUES (:title, :author, :isbn, :published_date , :category, :status, :price, :owner)";
+
+        $connection = $this->getEntityManager()->getConnection();
+        $statement = $connection->prepare($sql);
+        $statement->bindValue('title', $title);
+        $statement->bindValue('author', $author);
+        $statement->bindValue('isbn', $isbn);
+        $statement->bindValue('published_date', $published_date);
+        $statement->bindValue('category', $category);
+        $statement->bindValue('status', $status);
+        $statement->bindValue('price', $price);
+        $statement->bindValue('owner', $owner);
+
+        $result = $statement->executeQuery();
+        return $result->fetchAllAssociative();
+    }
 }
 
 
